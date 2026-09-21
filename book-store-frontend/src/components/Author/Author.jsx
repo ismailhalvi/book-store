fb9e1dcd9e4eae00606ab4e11cd3fa87ed4e2816
ismailhalvi <smo3ahalvi30@gmail.com>
2026-09-21 16:15:23 +0300

    Fix author image URLs

--- book-store-frontend/src/components/Author/Author.jsx
+++ book-store-frontend/src/components/Author/Author.jsx
@@ -6,7 +6,9 @@ import { FaXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { Link } from "react-router-dom";
 function Author({ author }) {
-  const AuthorImg = author.image?.url;
+const AuthorImg = author.image?.url?.startsWith("http")
+  ? author.image.url
+  : `https://book-store-bkc3.onrender.com${author.image?.url}`;
   return (
     <div className="author col  mb-3">
       <Link to={`/author/${author.documentId}`}>
